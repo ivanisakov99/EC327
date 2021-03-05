5464586ae6b051aa87e170b76184efd0d2ace7f9
Ivan Isakov <isakovi@bu.edu>
2021-03-05 18:40:56 -0500

    Update

---
+++ 3.3 Exam_1/Q3.h
@@ -0,0 +1,7 @@
+#ifndef Q3
+#define Q3
+
+float cos(float x, int y);
+//Calculates the Taylor expansion of the cosine of angle x (express in radians). The function calculates the Taylor series up to element y
+
+#endif
